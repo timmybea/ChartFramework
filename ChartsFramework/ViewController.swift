875eb93c6f935beb558c45d8fb6e2256ccecf13a
timmybea <tim.beals@gmail.com>
2017-11-13 08:51:46 -0800

    Setup viewcontroller with chart view and a button to send data

--- ChartsFramework/ViewController.swift
+++ ChartsFramework/ViewController.swift
@@ -7,21 +7,53 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    let chartView: PieChartView = {
+        let view = PieChartView()
+        view.backgroundColor = UIColor.blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Send Data", for: .normal)
+        button.backgroundColor = UIColor.red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = UIColor.white
+        setupSubviews()
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupSubviews() {
+        
+        view.addSubview(chartView)
+        chartView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        chartView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        chartView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        chartView.heightAnchor.constraint(equalToConstant: view.bounds.width).isActive = true
+     
+        view.addSubview(button)
+        button.topAnchor.constraint(equalTo: chartView.bottomAnchor, constant: 8).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.addTarget(self, action: #selector(buttonTouched(sender:)), for: .touchUpInside)
     }
 
 
+    @objc func buttonTouched(sender: UIButton) {
+        
+        print("Yowzaa")
+    }
+    
 }
 
