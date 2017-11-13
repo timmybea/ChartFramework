b1d1ad7c9ce5adc8cca62bd1e5bf49ce10913d49
timmybea <tim.beals@gmail.com>
2017-11-13 08:06:52 -0800

    Initial commit. Setup projects and import the chart framework

---
+++ ChartsFramework/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChartsFramework
+//
+//  Created by Tim Beals on 2017-11-13.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ChartsFramework' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ChartsFramework
+pod 'Charts', '~> 3.0'
+  target 'ChartsFrameworkTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ChartsFrameworkUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
